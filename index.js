e50b5f59bc5d7f5dc328ac20c380953623b6b5af
Rey-Cy <reycristiancy@gmail.com>
2024-02-01 00:29:40 -0500

    import router and use routes

--- index.js
+++ index.js
@@ -1,20 +1,13 @@
 import express from 'express';
+import router from './routes/index.js';
 
 const app = express();
 
 const port = process.env.PORT || 4000;
 
-app.get('/', (req, res)=>{
-    res.send('hello word');// req : lo que el usuario nos envia - res : lo que express responde a la solicitud de usuario  
-});
+app.set('view engine', 'pug');
 
-app.get('/contactos', (req, res)=>{
-    res.send('contactanos');
-});
-
-app.get('/sobre-mi', (req, res)=>{
-    res.send('Todo lo que quieras saber sobre mí');
-});
+app.use('/', router);
 
 app.listen(port,()=>{
     console.log(`Example app listening on port ${port}`);
