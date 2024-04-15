47c32ea5ef82ca7aa9d94329da80193e81b09fb1
Rey-Cy <reycristiancy@gmail.com>
2024-04-14 22:44:13 -0500

    feat: :lipstick: Add testimonials trips views and create routes
    Views were created for the testimonial and travel links and routes were created for each one and the names of each view were passed as a variable.

--- routes/index.js
+++ routes/index.js
@@ -3,17 +3,41 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', (req, res)=>{
-    res.render('home');// req : lo que el usuario nos envia - res : lo que express responde a la solicitud de usuario  
+    
+    const page = 'Inicio';
+
+    res.render('home',{
+        page 
+    });// req : lo que el usuario nos envia - res : lo que express responde a la solicitud de usuario  
 });
 
 router.get('/about-us', (req, res)=>{
 
-    const trip = 'Argentina';
+    const page = 'Acerca de Nostoros';
 
     res.render('about-us', {
-        trip
+        page
     });// req : lo que el usuario nos envia - res : lo que express responde a la solicitud de usuario  
 });
+
+router.get('/trips', (req, res)=>{
+
+    const page = 'Viajes';
+
+    res.render('trips', {
+        page
+    });// req : lo que el usuario nos envia - res : lo que express responde a la solicitud de usuario  
+});
+
+router.get('/testimonials', (req, res)=>{
+
+    const page = 'Testimoniales';
+
+    res.render('testimonials', {
+        page
+    });// req : lo que el usuario nos envia - res : lo que express responde a la solicitud de usuario  
+});
+
 
 export default router;
 
