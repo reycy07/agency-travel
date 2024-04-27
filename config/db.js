76844c38adb028ffc612602191563774fcae508e
Rey-Cy <reycristiancy@gmail.com>
2024-04-27 00:10:03 -0500

    feat: :sparkles: create db.js for connection
    conxion througth sequelize

---
+++ config/db.js
@@ -0,0 +1,18 @@
+import { Sequelize } from "sequelize";
+
+const db = new Sequelize('agenciaviajes', 'root', '',{
+    host: 'localhost',
+    port: '3306',
+    dialect: 'mysql',
+    define:{
+        timestamps: false
+    },
+    pool:{
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    },
+});
+
+export default db;
